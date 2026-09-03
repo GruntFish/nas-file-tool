aca4546f1456c10b1dc3343739a0b08aea49e3fc
GruntFish <165454237+GruntFish@users.noreply.github.com>
2026-09-03 12:34:59 +0800

    Update chmod.js

--- static/js/modules/chmod.js
+++ static/js/modules/chmod.js
@@ -1,5 +1,9 @@
 // static/js/modules/chmod.js
 
+if (typeof ModuleRegistry === 'undefined') {
+    var ModuleRegistry = window.ModuleRegistry || window.ModuleManager || {};
+}
+
 const ChmodModule = {
     name: 'chmod',
 
