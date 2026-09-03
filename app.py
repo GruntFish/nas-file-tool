230db40c135808ce15aad03a0b6583a2a1204ee9
GruntFish <165454237+GruntFish@users.noreply.github.com>
2026-09-03 10:07:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from core import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8658, debug=False)
