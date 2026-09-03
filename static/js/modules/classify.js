2bf0ae061d78c27f9b7e34f50c82fcdcb89e3588
GruntFish <165454237+GruntFish@users.noreply.github.com>
2026-09-03 15:24:10 +0800

    Update classify.js

--- static/js/modules/classify.js
+++ static/js/modules/classify.js
@@ -2,6 +2,18 @@
 const ClassifyModule = {
     name: 'classify',
 
+    destroy() {
+    // 如果有弹窗，关闭弹窗
+    closeModal();
+    // 清空选中
+    selectedFiles.clear();
+    updateSelectedInfo();
+    // 刷新文件列表
+    if (typeof renderFiles === 'function' && window.fileList) {
+        renderFiles(window.fileList);
+        }
+    }
+    
     init() {
         document.getElementById('classifyOpenBtn').addEventListener('click', () => this.openModal());
         this.updateCount();
