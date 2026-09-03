5b044c90079cb28204e06912810643fcb4e8ae2e
GruntFish <165454237+GruntFish@users.noreply.github.com>
2026-09-03 12:35:48 +0800

    Update media.js

--- static/js/modules/media.js
+++ static/js/modules/media.js
@@ -1,5 +1,9 @@
 // static/js/modules/media.js
 
+if (typeof ModuleRegistry === 'undefined') {
+    var ModuleRegistry = window.ModuleRegistry || window.ModuleManager || {};
+}
+
 const MediaModule = {
     name: 'media',
 
